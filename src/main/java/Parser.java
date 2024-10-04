0ba183259abb0d10e076d9321e4df8c890bb0430
KhweziN <khwezisandi@gmail.com>
2024-10-04 16:58:21 +0200

    enhanced structure to Parser file

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,42 +1,44 @@
-// import java.io.File;
-// import java.util.Stack;
+import java.io.File;
+import java.util.Stack;
+import java.util.List;
 
 
-// public class Parser {
-//     ParseTable parseTable;
-//     private final Stack<Integer> stack = new Stack<>();
-//     private String input;
-//     private int index = 0;
+public class Parser {
+    ParseTable parseTable;
+    private final Stack<Integer> stack = new Stack<>();
+    private String input;
+    private int index = 0;
+    private List<Token> tokens;
 
-//     Parser(ParseTable parseTable){
-//         this.parseTable = parseTable;
-//     }
+    Parser(ParseTable parseTable){
+        this.parseTable = parseTable;
+    }
 
-//     Private Token readNextToken(){
-//         // reads the next token
-//     }
+    private Token readNextToken(){
+        // reads the next token
+    }
 
 
-//     // stack := empty ; push(0,stack) ; read(next)
-//     // loop
-//     // case table[top(stack),next] of
-//     //             shift s: push(s,stack) ;
-//     //             read(next)
+    // stack := empty ; push(0,stack) ; read(next)
+    // loop
+    // case table[top(stack),next] of
+    //             shift s: push(s,stack) ;
+    //             read(next)
 
-//     //     reduce p: n := the left-hand side of production p ;
-//     //                     r := the number of symbols
-//     //                     on the right-hand side of p ;
-//     //                     pop r elements from the stack ;
-//     //                     push(s,stack)
-//     //                     where table[top(stack),n] = go s
-//     // accept: terminate with success
-//     // error: reportError
-//     // endloop
-//     void parse(File tokenFile){
-//         // parse token file
-//         // validate that input is indeed in the grammar
-//         // resolve any conflicts/ambiguities by means of precedence
-//         // create parse tree
-//         // output [tree?] as xml 
-//     }
-// }
+    //     reduce p: n := the left-hand side of production p ;
+    //                     r := the number of symbols
+    //                     on the right-hand side of p ;
+    //                     pop r elements from the stack ;
+    //                     push(s,stack)
+    //                     where table[top(stack),n] = go s
+    // accept: terminate with success
+    // error: reportError
+    // endloop
+    void parse(File tokenFile){
+        // parse token file
+        // validate that input is indeed in the grammar
+        // resolve any conflicts/ambiguities by means of precedence
+        // create parse tree
+        // output [tree?] as xml 
+    }
+}
