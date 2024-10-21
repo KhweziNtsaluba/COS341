363acb87f57366f8de14d7bf82e1fff73d626caf
KhweziN <khwezisandi@gmail.com>
2024-10-22 01:23:27 +0200

    fixed issue with matching type classes to parse table equivalent

--- src/main/java/ASTNode.java
+++ src/main/java/ASTNode.java
@@ -1,25 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class ASTNode {
-    private String value;
-    private List<ASTNode> children;
-
-    public ASTNode(String value) {
-        this.value = value;
-        this.children = new ArrayList<>();
-    }
-
-    public void addChild(ASTNode child) {
-        children.add(child);
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder(value);
-        if (!children.isEmpty()) {
-            sb.append(" -> ").append(children);
-        }
-        return sb.toString();
-    }
+public interface ASTNode {
+    public void addChild(ASTNode child);
+    public String toString();
+    public List<ASTNode> getChildren();
 }
